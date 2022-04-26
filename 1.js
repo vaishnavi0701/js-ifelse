235a0971f181e7f421105626b8ebe4ece3706665
vaishnavi0701 <90175670+vaishnavi0701@users.noreply.github.com>
2022-04-26 22:39:49 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,511 @@
+// let r = require("readline-sync");
+// var num=r.question("enter a number");
+// if (num%2==0){
+//     console.log(num,"even")
+// }
+// else{
+//     console.log(num,"odd")
+// }
+
+
+// var a=require("readline-sync");
+// var b=a.question("enter num : ")
+// var c=require("readline-sync")
+// var d=c.question("enter num : ")
+// if (b>d){
+//     console.log(b,"max")
+// }
+// else{
+//     console.log(d,"max")
+// }
+
+
+// var a=require("readline-sync");
+// var b=a.question("enter num : ")
+// if (b>0){
+//     console.log(b,"positive")
+// }
+// else if (b<0){
+//     console.log(b,"negative")
+// }
+// else{
+//     console.log(b,"zero")
+// }
+
+
+// var a=require("readline-sync")
+// var b=a.question("enter num : ")
+// if (b%5==0 && b%11==0){
+//     console.log(b,"div by 5 and 11")
+// }
+// else{
+//     console.log(b,"not div")
+// }
+
+
+// var a=require("readline-sync")
+// var b=a.question("enter year : ")
+// if (b%4==0 && b%100!=0 || b%400==0){
+//     console.log(b,"leap year")
+// }
+// else{
+//     console.log(b,"not leap year")
+// }
+
+
+// var a=require("readline-sync")
+// var b=a.question("enter alphabets : ")
+// if (b>="a" && b<="z" || b>="A" && b<="Z"){
+//     console.log(b,"alphabet")
+// }
+// else{
+//     console.log(b,"not alphabet")
+// }
+
+// var a=require("readline-sync")
+// var b=a.question("enter alphabets : ")
+// if (b=="a" || b=="e" || b=="i" || b=="o" || b=="u"){
+//     console.log(b,"vowels")
+// }
+// else{
+//     console.log(b,"consonants")
+// }
+
+
+
+// var a=require("readline-sync")
+// var b=a.question("enter anything : ")
+// if (b>="a" && b<="z" || b>="A" && b<="Z"){
+//     console.log(b,"alphabet")
+// }
+// else if (b>="0" && b<="9"){
+//     console.log(b,"numbers")
+// }
+// else if (b=="&" || b=="@" || b=="$"){
+//     console.log(b,"special characters")
+// }
+// else{
+//     console.log("nothing")
+// }
+
+
+// var a=require("readline-sync")
+// var b=a.question("enter alphabets : ")
+// if (b>="a" && b<="z"){
+//     console.log(b,"lower case")
+// }
+// else if (b>="A" && b<="Z"){
+//     console.log(b,"upper case")
+// }
+
+
+// var num1 = 300 - 123;
+// let r = require("readline-sync");
+// var num2=r.question("enter a number");
+// if(num1==num2){
+//    console.log("equal")
+// }
+// else{
+//    console.log("not equal")
+// }
+
+
+// var num1 = 44 * 30;
+// let r = require("readline-sync");
+// var num2=r.questionInt("enter a number");
+// if(num2>=num2){
+//    console.log("number2 is greater than or equal");
+// }
+// else{
+//    console.log("number2 is lesser");
+// }
+
+
+
+// var n=require("readline-sync")
+// var day=n.question("enter the day : ");
+
+// if (day == "monday") {
+//   console.log("Dhanwantari\nSiddhi\nKritika\nSalon\nMayuri\nGauri".toLowerCase());
+// }
+// else if (day == "tuesday") {
+//   console.log("Keemaya\nRoshni\nPooja\Priyanka\nnalini\nManisha".toLowerCase());
+// }
+// else if (day == "wednesday") {
+//   console.log("Shrusti\nKaruna\nParveen\nSandhya\nLeena\nKarishma".toLowerCase());
+// }
+// else if (day == "thursday") {
+//   console.log("Nikita\nMona\nPriyanka\nTejashree\nLovely\nMehzebin".toLowerCase());
+// }
+// else if (day == "friday") {
+//   console.log("Pihu\nSejal\nChaya\nShrestha\nMonali\nAardhangya".toLowerCase());
+// }
+// else if (day == "saturday") {
+//   console.log("Gunjan\nShweta\nRani\nIsha\nKusu\Madhu\n".toLowerCase());
+// }
+// else if (day == "sunday") {
+//   console.log("Swati\nKhusboo\nidhi\Preeti\nDipti\nAnamika".toLowerCase());
+// }
+// else {
+//   console.log("Please enter valid week day")
+// }
+
+
+// let r= require("readline-sync");
+// var num=r.questionInt("enter a number");
+// if (num<10){
+//    console.log("num is less than 10");
+// }
+// else if(num>10 && num<20){
+//    console.log("num is less than 20");
+// }
+// else{
+//    console.log("greater than 20");
+// }
+
+
+// let r = require("readline-sync");
+// var water=r.questionInt("enter water level");
+// if (water<1){
+//    console.log("more water needs to fill");
+// }
+// else if (water>=1 && water<=10){
+//    console.log("no need to fill water");
+// }
+// else{
+//    console.log("overflow");
+// }
+
+
+
+// var a=require("readline-sync")
+// var b=a.question("enter week num : ")
+// if (b=="1"){
+//     console.log("monday")
+// }
+// else if (b=="2"){
+//     console.log("tuesday")
+// }
+// else if (b=="3"){
+//     console.log("wednesday")
+// }
+// else if (b=="4"){
+//     console.log("thursday")
+// }
+// else if (b=="5"){
+//     console.log("friday")
+// }
+// else if (b=="6"){
+//     console.log("saturday")
+// }
+// else if (b=="7"){
+//     console.log("sunday")
+// }
+
+
+// var a=require("readline-sync")
+// var b=a.question("enter sides of triangle : ")
+
+// var c=require("readline-sync")
+// var d=c.question("enter sides of triangle : ")
+
+// var e=require("readline-sync")
+// var f=e.question("enter sides of triangle : ")
+
+// if (b==d && d==e && e==b){
+//     console.log("equilateral triangle")
+// }
+// else if (b==d || d==e || e==b){
+//     console.log("isosceles triangle")
+// }
+// else{
+//     console.log("scalene triangle")
+// }
+
+
+
+// var a=require("readline-sync")
+// var num1=a.question("enter num : ")
+
+// var c=require("readline-sync")
+// var num2=c.question("enter num : ")
+
+// var e=require("readline-sync")
+// var num3=e.question("enter num : ")
+
+// if (num1>num2 && num1>num3){
+//     console.log(num1,"greater number")
+// }
+// else if (num2>num1 && num2>num3){
+//     console.log(num2,"greater number")
+// }
+// else if (num3>num1 && num3>num2){
+//     console.log(num3,"greater number")
+// }
+
+
+// var a=require("readline-sync")
+// var b=a.question(" enter birth year : ")
+
+// var c=require("readline-sync")
+// var d=c.question("enter current year : ")
+// if (b<d){
+//     console.log(d-b)
+// }
+// else{
+//     console.log("not any")
+// }
+
+// var a=require("readline-sync")
+// var b =a.question("enter number : ")
+// c=b%10
+// if (c>0){
+//     console.log(c,"last digit")
+// }
+// else{
+//     console.log(c,"not last digit")
+// }
+
+
+// var a=require("readline-sync")
+// var b =a.question("enter number : ")
+// c=b%10
+// if (c%2==0){
+//     console.log(c,"div by 2")
+// }
+// else{
+//     console.log(c,"not divisible")
+// }
+
+// var a=5
+// var b=(a>15); ? ,{"pass":"fail"};
+// console.log("you have",b)
+
+
+// program to check pass or fail
+
+// let marks=70
+
+// // check the condition
+// let result = (marks >= 40) ? 'pass' : 'fail';
+
+// console.log(`You ${result} the exam.`);
+
+
+
+// var a=10
+// var b=(a>15) ? "pass":"fail";
+// console.log(`you are ${b}`)
+
+
+// var a=require("readline-sync")
+// var time =a.question("enter time : ");
+// if (time >= 5 && time <= 6) {
+//  console.log("sleep well");
+// }
+// else if (time >= 6 && time <= 7) {
+//  console.log("Morning Exercise");
+// }
+// else if(time > 7 && time < 8.30){
+//  console.log("Break+Breakfast");
+// }
+// else if ( time > 8.30 && time < 12){
+//  console.log("Study");
+// }
+
+
+
+// var x = 10;
+// var y = 12
+// var z = 90
+ 
+// if (x > y){
+//   if (x < z){
+//       x = y
+//   }
+//   else{
+//       m = z
+//   }
+// }
+// else {
+//   if(y > z){
+//       m = y
+//   }
+//   else {
+//       m = x
+//   } 
+// }
+// console.log(m)
+
+
+
+// var choice ="overcast";
+// var message ="" 
+// if (choice === 'sunny') {
+//     message = 'It is nice and sunny outside today. \nWear shorts! Go to the beach, or the park, and get an ice cream.'
+//  } else if (choice === 'rainy') {
+//      message = 'Rain is falling outside; \nTake a rain coat and an umbrella, and don\'t stay out for too long.'
+//  } else if (choice === 'snowing') {
+//      message = 'The snow is coming down — it is freezing! \nBest to stay in with a cup of hot chocolate, or go build a snowman.'
+//  } else if (choice === 'overcast') {
+//      message = 'It isn\'t raining, but the sky is grey and gloomy; \nit could turn any minute, so take a rain coat just in case.'
+//  } else {
+//    message = 'Nothing'
+// }
+// console.log(message)
+
+
+// var z=require("readline-sync")
+// var b=z.question("enter num : ")
+// var a=300-123
+// if (a==b){
+//     console.log("equal")
+// }
+// else{
+//     console.log("not equal")
+// }
+
+// var r = require("readline-sync");
+// var age=r.questionInt("enter age")
+// if(age>=5){
+//    console.log("you can go school")
+// }
+// if(age>=18){
+//    console.log("you can vote in elections.")
+// }
+// if(age>=21){
+//    console.log("you can drive a car")
+// } 
+// if(age>=24){
+//    console.log("you can marry")
+// }
+// if(age>=25){
+//    console.log("you can drink")
+// }
+
+
+// var z=require("readline-sync")
+// var a=z.question("enter num : ")
+// var b=z.question("enter num : ")
+// var c=z.question("enter num : ")
+// if(a>b && a>c){
+//     console.log(a,"max")
+// }
+// else if(b>a && b>c){
+//     console.log(b,"max")
+// }
+// else if(c>a &&c>b){
+//     console.log(c,"max")
+// }
+
+// var z=require("readline-sync")
+// var a=z.question("enter num : ")
+// if(a>0){
+//     console.log(a,"positive")
+// }
+// else if(a<0){
+//     console.log(a,"negative")
+// }
+// else if(a==0){
+//     console.log(a,"neutral")
+// }
+
+// var z=require("readline-sync")
+// var a=z.question("enter year : ")
+// if(a%4==0 && a%400==0 && a%100==0){
+//     console.log(a,"leap year")
+// }
+// else{
+//     console.log(a,"not leap year")
+// }
+
+// var z=require("readline-sync")
+// var a=z.question("enter alphabets : ")
+// if(a>="a" && a<="z" || a>="A" && a<="Z"){
+//     console.log(a,"alphabets")
+// }
+// else{
+//     console.log(a,"not alphabets")
+// }
+
+// var z=require("readline-sync")
+// var a=z.question("enter alphabets :  ")
+// if(a=="a" || a=="e" | a=="i" || a=="o" || a=="u"){
+//     console.log(a,"vowel")
+// }
+// else{
+//     console.log(a,"consonants")
+// }
+
+// var z=require("readline-sync")
+// var a=z.question("enter characters :  ")
+// if(a>="a" && a<="z" || a>="A" && a<="Z"){
+//     console.log(a,"alpbhabets")
+// }
+// else if(a=="1" || a=="2" || a=="3" || a=="4" || a=="5" || a=="6" || a=="7" || a=="8" || a=="9"|| a=="0"){
+//     console.log(a,"digits")
+// }
+// else if(a=="@" || a=="#" || a=="$" || a=="&"){
+//     console.log(a,"special characters")
+// }
+
+// var z=require("readline-sync")
+// var a=z.question("enter alphabets :  ")
+// if(a>="a" && a<="z"){
+//     console.log(a,"lower case")
+// }
+// else if(a>="A" && a<="Z"){
+//     console.log(a,"upper case")
+// }
+
+
+// var z=require("readline-sync")
+// var a=z.question("enter values :  ")
+// var b=z.question("enter values :  ")
+// var c=z.question("enter values :  ")
+// if(a==b==c){
+//     console.log("equilateral tr")
+// }
+// else if(a==b && b==c && c==a){
+//     console.log("isosceles tr")
+// }
+// else{
+//     console.log("scalene tr")
+// }
+
+
+// var z=require("readline-sync")
+// var a=z.question("enter cp : ")
+// var b=z.question("enetr sp : ")
+// if(a>b){
+//     console.log(a-b,"profit")
+// }
+// else if(b>a){
+//     console.log(b-a,"loss")
+// }
+// else if(a==b){
+//     console.log(a,"no profit no loss")
+// }
+
+
+
+// var z=require("readline-sync")
+// var percentage=z.question("enter percentage :  ")
+// if(percentage >= 90){
+//     console.log(percentage,"Grade A")
+// }
+// else if(percentage>=80){
+//     console.log(percentage,"Grade B")
+// }
+// else if(percentage>=70){
+//     console.log(percentage,"Grade C")
+// }
+// else if(percentage>=60){
+//     console.log(percentage,"Grade D")
+// }
+// else if(percentage>=40){
+//     console.log(percentage,"Grade E")
+// }
+// else if(percentage<40){
+//     console.log(percentage,"Grade F")
+// }
